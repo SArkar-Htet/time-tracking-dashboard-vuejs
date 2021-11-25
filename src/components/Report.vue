ebe7101c723e53bf31c5f3d26e6bf4e0eacc3501
Soe Arkar Htet <developer.soearkarhtet@gmail.com>
2021-11-25 21:22:21 +0630

    refactor: modify to get backend

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -23,113 +23,21 @@
     methods: {
       getData(timeFrame) {
         this.timeFrame = timeFrame;
+      },
+      async fetchData() {
+        const url = "http://localhost:3000/reports";
+        try {
+          const res = await fetch(url);
+          const reports = await res.json();
+          console.table(reports);
+          return reports;
+        } catch (error) {
+          console.log(error);
+        }
       }
     },
-    created() {
-      this.reports = [
-        {
-          "title": "Work",
-          "timeframes": {
-            "daily": {
-              "current": 5,
-              "previous": 7
-            },
-            "weekly": {
-              "current": 32,
-              "previous": 36
-            },
-            "monthly": {
-              "current": 103,
-              "previous": 128
-            }
-          }
-        },
-        {
-          "title": "Play",
-          "timeframes": {
-            "daily": {
-              "current": 1,
-              "previous": 2
-            },
-            "weekly": {
-              "current": 10,
-              "previous": 8
-            },
-            "monthly": {
-              "current": 23,
-              "previous": 29
-            }
-          }
-        },
-        {
-          "title": "Study",
-          "timeframes": {
-            "daily": {
-              "current": 0,
-              "previous": 1
-            },
-            "weekly": {
-              "current": 4,
-              "previous": 7
-            },
-            "monthly": {
-              "current": 13,
-              "previous": 19
-            }
-          }
-        },
-        {
-          "title": "Exercise",
-          "timeframes": {
-            "daily": {
-              "current": 1,
-              "previous": 1
-            },
-            "weekly": {
-              "current": 4,
-              "previous": 5
-            },
-            "monthly": {
-              "current": 11,
-              "previous": 18
-            }
-          }
-        },
-        {
-          "title": "Social",
-          "timeframes": {
-            "daily": {
-              "current": 1,
-              "previous": 3
-            },
-            "weekly": {
-              "current": 5,
-              "previous": 10
-            },
-            "monthly": {
-              "current": 21,
-              "previous": 23
-            }
-          }
-        },
-        {
-          "title": "Self Care",
-          "timeframes": {
-            "daily": {
-              "current": 0,
-              "previous": 1
-            },
-            "weekly": {
-              "current": 2,
-              "previous": 2
-            },
-            "monthly": {
-              "current": 7,
-              "previous": 11
-            }
-          }
-        }
-      ];
+    async created() {
+      this.reports = await this.fetchData();
     }
   }
 </script>
